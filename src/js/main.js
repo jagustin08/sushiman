631a476ea8bb2133ca07de2c74d917c4c63d2b02
Jayson Agustin <dev.jagustin@gmail.com>
2023-07-11 16:37:04 +0800

    add content

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper("#testimonialSwiper", {
+    spaceBetween: 30,
+    autoHeight: true,
+    loop: true,
+    autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+    },
+});
