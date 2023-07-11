19a9e51b65c49329821f4a3961111d18df98a67c
Jayson Agustin <dev.jagustin@gmail.com>
2023-07-11 15:11:27 +0800

    add import swiper

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import Swiper from 'swiper';
+
+var swiper = new Swiper("#testimonialSwiper", {
+    spaceBetween: 30,
+    autoHeight: true,
+    loop: true,
+    autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+    },
+});
